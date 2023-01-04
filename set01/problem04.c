1961e60f9ce11bb8e4a854d1421f649a44f7b89b
Saniya Khanum  Khanum <114483109+saniyakhanum@users.noreply.github.com>
2023-01-04 13:37:35 +0000

    solved

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,33 +1,33 @@
- `1#include <stdio.h>
-    int input();
-    void add(int a, int b, int *sum);
-    void output(int a, int b, int sum);
-    
-       
-    int input()
-    {
-    int n;
-       printf("Enter a number");
-       scanf("%d",&n);
-       return n;
-    }
+#include <stdio.h>
+int input();
+void add(int a, int b, int *sum);
+void output(int a, int b, int sum);
 
-    void add(int a, int b, int *sum)
-    {
-     *sum=a+b;
-    }
-
-    void output(int a, int b, int sum)
-    {
-       printf("The sum of %d and %d is %d",a,b,sum);
-    }
-
-    int main()
-  {
-     int a, b, sum;
-      a=input();
-      b=input();
-   add(a,b,&sum);
-   output (a,b,sum);
-   return 0;
+int input()
+{
+  int n;
+  printf("Enter a number");
+  scanf("%d",&n);
+  return n;
 }
+
+void add (int a, int b, int *sum)
+{
+  *sum=a+b;
+}
+
+void output(int a, int b, int sum)
+{
+  printf("The sum of %d and %d is %d",a,b,sum);
+}
+
+int main()
+{
+  int a,b,*sum;
+  a=input();
+  b=input();
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
+}
+
