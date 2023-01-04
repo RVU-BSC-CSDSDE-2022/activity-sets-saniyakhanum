94462eacb9b1e676ed6bb518e9504a27ea4ea515
Saniya Khanum  Khanum <114483109+saniyakhanum@users.noreply.github.com>
2023-01-04 13:56:06 +0000

    solved

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 int input();
 int compare(int a, int b, int c);
 void output(int a, int b, int c, int largest);
@@ -6,11 +6,12 @@ void output(int a, int b, int c, int largest);
 int input()
 {
   int n;
-  printf("enter the number:");
+  printf("Enter a number");
   scanf("%d",&n);
   return n;
 }
-int compare(int a , int b ,int c)
+
+int compare(int a, int b, int c)
 {
   int largest;
   if(a>b && a>c){
@@ -23,18 +24,22 @@ int compare(int a , int b ,int c)
     largest=c;
   }
 
-}
-void output(int a, int b, int c, int largest)
-{
-  printf("largest no %d, %d ,%d is %d",a,b,c,largest);
-}
-int main()
-{
-  int a,b,c,largest;
-  a=input();
-  b=input();
-  c=input();
-  compare(a,b,c);
-  output(a,b,c,largest);
-  return(0);
-}
+  void output(int a, int b, int c, int largest)
+  {
+    printf("The largest of %d %d %d is %d",a,b,c,largest);
+  }
+
+  int main()
+  {
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
+  }
+} 
+
+
+
