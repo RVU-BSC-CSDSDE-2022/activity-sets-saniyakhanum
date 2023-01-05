c39a266c3bf6f8261543c1e11d5fcd8cf6cc5cef
Saniya Khanum  Khanum <114483109+saniyakhanum@users.noreply.github.com>
2023-01-05 09:23:37 +0000

    solved

--- set01/problem07.c
+++ set01/problem07.c
@@ -7,32 +7,30 @@ void output(int n, int sum);
 int input()
 {
   int n;
-  printf("Enter the number\n");
-  scanf("%d", &n);
+  printf("Enter a number");
+  scanf("%d",&n);
   return n;
 }
 
 int sum_n_nos(int n)
 {
-    int sum=0;
-    for(int i=1; i<n; i++)
-      {
-        sum=sum+i;
-      }
-
-  return sum;
+  int sum=0,i;
+  for(i=0;i<n ; i++)
+    sum=sum+i;
+    return sum;
 }
 
-void output(int n , int sum)
+void output(int n, int sum)
 {
-  printf("The sum of %d is %d\n",n, sum);
+  printf("The sum of %d is %d ",n,sum);
 }
 
 int main()
 {
-  int n, sum=0;
+  int n,sum;
   n=input();
   sum=sum_n_nos(n);
-  output(n, sum);
-  return(0);
+  output(n,sum);
+  return 0;
 }
+
