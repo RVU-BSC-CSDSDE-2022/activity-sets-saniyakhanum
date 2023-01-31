18c950f7eb91a44272b4483d77aa2d050755119d
Saniya Khanum  Khanum <114483109+saniyakhanum@users.noreply.github.com>
2023-01-31 02:02:40 +0000

    solved

--- set01/problem05.c
+++ set01/problem05.c
@@ -5,41 +5,46 @@ void output(int a, int b, int c, int largest);
 
 int input()
 {
-  int n;
-  printf("Enter a number");
-  scanf("%d",&n);
-  return n;
+int n;
+printf("Enter a number");
+scanf("%d",&n);
+return n;
 }
 
-int compare(int a, int b, int c)
+int compare(int a ,int b, int c)
 {
-  int largest;
-  if(a>b && a>c){
-    largest=a;
-  }
-  if(b>a && b>c){
-    largest=b;
-  }
-  else{
-    largest=c;
+int largest;
+if(a>b && a>c){
+largest=a;
+}
+
+if (b>a && b>c){
+largest=b;
+}
+
+else {
+largest=c;
   }
 
-  void output(int a, int b, int c, int largest)
-  {
-    printf("The largest of %d %d %d is %d",a,b,c,largest);
-  }
+return largest;
+}
 
-  int main()
-  {
-    int a,b,c,largest;
-    a=input();
-    b=input();
-    c=input();
-    compare(a,b,c);
-    output(a,b,c,largest);
-    return 0;
-  }
-} 
+void output(int a, int b, int c, int largest)
+{
+printf("The largest of %d %d %d is %d",a,b,c,largest);
+}
+
+int main()
+{
+int a, b,c, largest;
+a=input();
+b=input();
+c=input();  
+largest=compare(a,b,c);
+output(a,b,c,largest);
+return 0;
+}
+
 
 
 
--- set01/problem06.c
+++ set01/problem06.c
@@ -1,43 +1,46 @@
-#include<stdio.h>
+#include <stdio.h>
 int input();
 void compare(int a, int b, int c, int *largest);
 void output(int a, int b, int c, int largest);
-
 int input()
 {
-  int n;
-  printf("Enter a number");
-  scanf("%d",&n);
-  return n;
+int n;
+printf("Enter a number");
+scanf("%d",&n);
+return n;
 }
 
 void compare(int a, int b, int c, int *largest)
 {
-  if(a>b && a>c)
-    *largest=a;
- 
-  if (b>a && b>c)
-    *largest=b;
+if(a>b && a>c){
+*largest =a;
+}
 
-  else
-    *largest=c;
+if (b>a && b>c){
+*largest=b;
+}
+
+else {
+*largest=c;
+}
 }
 
 void output(int a, int b, int c, int largest)
 {
-  printf("The largest of %d %d %d is %d",a,b,c,largest);
+printf("The largest of %d %d %d is %d",a,b,c,largest);
 }
 
 int main()
 {
-  int a,b,c,largest;
-  a=input();
-  b=input();
-  c=input();
-  compare(a,b,c,&largest);
-  output(a,b,c,largest);
-  return 0;
+int a, b,c, *largest;
+a=input();
+b=input();
+c=input();
+compare(a,b,c,&largest);
+output(a,b,c,largest);
+return 0;
 }
+
 
 
 
